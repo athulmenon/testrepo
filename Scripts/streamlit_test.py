2537d679c97322e4af3f71464a57c9be0d597d1f
Athul Menon K <athulmenonk@gmail.com>
2024-05-12 12:56:42 +0100

    Testing again

--- Scripts/streamlit_test.py
+++ Scripts/streamlit_test.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.write("Hello world")
-movie = st.text_input('Favorite Movie?')
-st.write(movie)
+#movie = st.text_input('Favorite Movie?')
+#st.write(movie)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a","b","c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
+
+
